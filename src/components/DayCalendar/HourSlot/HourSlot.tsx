a37b48c864ccc3e2aed78059685770f478058d37
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-12-16 15:42:38 +0100

    ADD - hour slot

---
+++ src/components/DayCalendar/HourSlot/HourSlot.tsx
@@ -0,0 +1,8 @@
+export default function HourSlot() {
+  return (
+    <div className="flex gap-2 flex-1">
+      <p className="text-sm pt-2">1 AM</p>
+      <div className="block flex-1  border border-b-0 border-stdSlateGray"></div>
+    </div>
+  );
+}
