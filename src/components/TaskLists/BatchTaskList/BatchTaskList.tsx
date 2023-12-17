d5b973f3ecc5a5f2bbb1905f736070549cfe7e1e
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-12-17 18:40:33 +0100

    ADD - Batch list calendar

---
+++ src/components/TaskLists/BatchTaskList/BatchTaskList.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState } from "react";
+
+interface Task {
+  id: string;
+  taskName: string;
+  isCompleted: boolean;
+}
+
+export default function BatchTaskList() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const taskSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formEvent = new FormData(event.currentTarget);
+
+    const taskName = formEvent.get("task");
+    const id = (Math.random() * 100000000).toFixed(0).toString();
+    const taskToAdd: Task = {
+      id,
+      isCompleted: false,
+      taskName: taskName?.toString() ?? "",
+    };
+    console.log("Tasks to add", taskToAdd);
+    setTasks((prevTasks) => [taskToAdd, ...prevTasks]);
+
+    event.currentTarget.reset();
+  };
+  return (
+    <div className="flex flex-col flex-1 p-4 ">
+      <h3>Batch Tasks</h3>
+      <form action="" className="flex gap-4" onSubmit={taskSubmitHandler}>
+        <input name="task" type="text" />
+        <button className="secondary-btn">Add</button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            className="flex border rounded-sm align-middle justify-between pl-4 pr-4"
+          >
+            <p className="mt-auto mb-auto">{task.taskName}</p>
+            <button>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
