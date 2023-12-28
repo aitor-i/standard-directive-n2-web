111f7616b45077cd9a1b54bc8f7110911cb8130e
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-12-28 18:59:50 +0100

    ADD - Edit daily task modal

--- src/components/TaskLists/DailyTaskList/DailyTaskListItem/DailyTasksListItem.tsx
+++ src/components/TaskLists/DailyTaskList/DailyTaskListItem/DailyTasksListItem.tsx
@@ -1,5 +1,6 @@
 import { HoursContext } from "@/contexts/hoursContext/hoursContext";
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
+import { EditDailyTaskItemForm } from "./EditDailyTaskItemForm/EditDailyTaskItemForm";
 
 interface Props {
   isCompleted: boolean;
@@ -13,6 +14,7 @@ export default function DailyTasksListItem({
   taskName,
 }: Props) {
   const { hours, onSetHours } = useContext(HoursContext);
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   const changeTaskCompletedStatus = () => {
     return hours.map((task) => {
@@ -29,13 +31,35 @@ export default function DailyTasksListItem({
     onSetHours(newHours);
   };
 
+  const dialogCloseHandler = () => {
+    dialogRef.current?.close();
+  };
+
+  const dialogOpenHandler = () => {
+    dialogRef.current?.showModal();
+  };
+
+  const onEditClickHandler = () => {
+    dialogOpenHandler();
+  };
+
   return (
     <div className="flex gap-4 p-2 border border-stdSlateGray m-1">
       <span className="cursor-pointer" onClick={onClickTask}>
         {isCompleted ? "X" : "O"}
       </span>
       <p className="flex-1">{taskName}</p>
-      <span className="cursor-pointer">!</span>
+      <span className="cursor-pointer" onClick={onEditClickHandler}>
+        !
+      </span>
+      <dialog className="w-1/2 rounded" ref={dialogRef}>
+        <EditDailyTaskItemForm
+          onDismiss={() => {}}
+          onSelect={() => {}}
+          onSubmit={() => {}}
+          taskId={taskId}
+        />
+      </dialog>
     </div>
   );
 }
---
+++ src/components/TaskLists/DailyTaskList/DailyTaskListItem/EditDailyTaskItemForm/EditDailyTaskItemForm.tsx
@@ -0,0 +1,108 @@
+import { HoursContext } from "@/contexts/hoursContext/hoursContext";
+import { Colors, colors } from "@/domain/colors/colors";
+import React, { useContext } from "react";
+
+interface Props {
+  onSubmit: () => void;
+  onSelect: () => void;
+  onDismiss: () => void;
+  taskId: number;
+}
+
+function findColorKey<T extends Record<string, string>>(
+  obj: T,
+  color: string
+): keyof T | undefined {
+  // Loop through the keys of the object.
+  for (const key in obj) {
+    if (obj[key] === color) {
+      // If the value matches the color, return the key.
+      return key as keyof T;
+    }
+  }
+  // Return undefined if the color isn't found.
+  return undefined;
+}
+
+export function EditDailyTaskItemForm({
+  onSelect,
+  onSubmit,
+  onDismiss,
+  taskId,
+}: Props) {
+  const { hours, onSetHours } = useContext(HoursContext);
+  const freeHours = hours;
+  const availableEndHours = hours;
+  const taskHours = hours.filter((hour) => hour.taskId === taskId);
+  const taskStartTime = taskHours.at(0);
+  const taskEndTime = taskHours.at(-1);
+  const taskName = taskHours.at(0)?.eventName;
+
+  const taskColor = findColorKey(colors, taskStartTime?.color ?? "");
+  console.log(taskColor);
+
+  return (
+    <div className="border rounded p-4">
+      <h4 className="text-xl mb-4 font-medium">Edit tasks</h4>
+      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+        <section className="flex flex-col">
+          <label htmlFor="task-name">Task Name</label>
+          <input
+            placeholder="Insert task name"
+            required
+            type="text"
+            name="task-name"
+            maxLength={25}
+            value={taskName}
+          />
+        </section>
+        <section className="flex flex-col">
+          <label htmlFor="start-time">Start Time</label>
+          <select name="start-time" onChange={onSelect}>
+            <option value={taskStartTime?.hour}>
+              {taskStartTime?.hourDisplay}
+            </option>
+            {freeHours.map((hour) => (
+              <option value={hour.hour} key={hour.hour}>
+                {hour.hourDisplay}
+              </option>
+            ))}
+          </select>
+        </section>
+        <section className="flex flex-col">
+          <label htmlFor="end-time">End time</label>
+          <select name="end-time">
+            <option value={taskEndTime?.hour}>
+              {taskEndTime?.hourDisplay}
+            </option>
+            {availableEndHours.map((hour) => (
+              <option key={hour.hour} value={hour.hour}>
+                {hour.hourDisplay}
+              </option>
+            ))}
+          </select>
+        </section>
+        <section className="flex flex-col">
+          <label htmlFor="color">Set Color</label>
+          <select name="color">
+            <option value={taskColor}>{taskColor}</option>
+            {Object.keys(colors).map((colorName) => (
+              <option key={colorName} value={colorName}>
+                {colorName}
+              </option>
+            ))}
+          </select>
+        </section>
+        <div className="flex gap-4 self-end">
+          <button type="submit" className="primary">
+            Save
+          </button>
+
+          <button type="reset" className="warning" onClick={onDismiss}>
+            Dismiss
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
