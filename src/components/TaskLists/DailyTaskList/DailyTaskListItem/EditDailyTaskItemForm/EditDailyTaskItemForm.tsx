0ff912b185231878c57896a391ca9191798e047c
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-12-30 18:04:14 +0100

    REFACTOR - Edit Daily task form logic into hook

--- src/components/TaskLists/DailyTaskList/DailyTaskListItem/EditDailyTaskItemForm/EditDailyTaskItemForm.tsx
+++ src/components/TaskLists/DailyTaskList/DailyTaskListItem/EditDailyTaskItemForm/EditDailyTaskItemForm.tsx
@@ -1,9 +1,6 @@
-import { CalendarHour } from "@/application/calendarDaysGenerator/calendarDaysGenerator";
-import { cleanEventInCalendar } from "@/application/setEventInCalendar/cleanEventInCalendar";
-import { setEventInCalendar } from "@/application/setEventInCalendar/setEventInCalendar";
-import { HoursContext, SetEventInCalendarContextProps } from "@/contexts/hoursContext/hoursContext";
-import { ColorKeys, Colors, colors } from "@/domain/colors/colors";
-import React, { useContext, useEffect, useState } from "react";
+import { colors } from "@/domain/colors/colors";
+import React from "react";
+import { useEditDailyTaskItemForm } from "./useEditDailyTaskItemForm";
 
 interface Props {
   onSubmit: () => void;
@@ -13,20 +10,6 @@ interface Props {
   isModalOpen: boolean
 }
 
-function findColorKey<T extends Record<string, string>>(
-  obj: T,
-  color: string
-): keyof T | undefined {
-  // Loop through the keys of the object.
-  for (const key in obj) {
-    if (obj[key] === color) {
-      // If the value matches the color, return the key.
-      return key as keyof T;
-    }
-  }
-  // Return undefined if the color isn't found.
-  return undefined;
-}
 
 export function EditDailyTaskItemForm({
   onSelect,
@@ -35,94 +18,8 @@ export function EditDailyTaskItemForm({
   taskId,
   isModalOpen
 }: Props) {
-  const { hours, onSetHours } = useContext(HoursContext);
-  const [availableEndHours, setAvailableEndOurs] = useState<CalendarHour[]>([])
-  const [freeHours, setFreeHours] = useState<CalendarHour[]>([])
 
-  const taskHours = hours.filter((hour) => hour.taskId === taskId);
-  const taskStartTime = taskHours.at(0);
-  const taskEndTime = taskHours.at(-1);
-  const taskName = taskHours.at(0)?.eventName;
-
-  const taskColor = findColorKey(colors, taskStartTime?.color ?? "");
-
-  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log('submit')
-    const formData = new FormData(event.currentTarget);
-    const startTime = formData.get("start-time");
-    const endTime = formData.get("end-time");
-    const taskName = formData.get("task-name");
-    const color = formData.get("color")! as string;
-
-    let colorValue = color as ColorKeys;
-
-    const setEventProps: SetEventInCalendarContextProps = {
-      color: colors[colorValue],
-      eventName: taskName as string,
-      timeStart: parseInt(startTime as string),
-      timeEnd: parseInt(endTime as string),
-    };
-
-    const cleanedHours = cleanEventInCalendar(taskId, hours)
-
-    const updatedHours = setEventInCalendar({
-      hours: cleanedHours,
-      taskId,
-      ...setEventProps,
-    });
-
-    onSetHours(updatedHours);
-
-    onSubmit();
-  }
-
-  const onDismissHandler = (event: React.FormEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    onDismiss()
-  };
-
-  const onDeleteHandler = (event: React.FormEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    const cleanedHours = cleanEventInCalendar(taskId, hours)
-    onSetHours(cleanedHours);
-  };
-
-  const onSelectTimeHandler = (event: React.SyntheticEvent<HTMLSelectElement>) => {
-    event.preventDefault();
-
-    const option = event.currentTarget.value;
-    const nextTasks = hours.filter(
-      (hour) => hour.hour > parseInt(option) && hour.color && hour.taskId !== taskId
-    );
-
-    const nextTaskTime = nextTasks[0]?.hour ?? 25;
-
-    const filteredEndHours = hours.filter(
-      (hour) =>
-        !hour.color && hour.hour >= parseInt(option) && hour.hour < nextTaskTime || hour.taskId === taskId && hour.hour >= parseInt(option)
-    );
-
-    setAvailableEndOurs(filteredEndHours);
-  }
-
-
-  useEffect(() => {
-    const nextTasks = hours.filter(
-      (hour) => hour.hour > taskStartTime?.hour! && hour.color && hour.taskId !== taskId
-    );
-
-    const nextTaskTime = nextTasks[0]?.hour ?? 25;
-
-    const filteredEndHours = hours.filter(
-      (hour) =>
-        !hour.color && hour.hour >= taskStartTime?.hour! && hour.hour < nextTaskTime || hour.taskId === taskId && hour.hour >= taskStartTime?.hour!
-    );
-    setAvailableEndOurs(filteredEndHours)
-
-    const filteredFreeHours = hours.filter(hour => !hour.color || hour.taskId === taskId)
-    setFreeHours(filteredFreeHours)
-  }, [isModalOpen])
+  const { taskName, taskEndTime, taskColor, freeHours, onDeleteHandler, onDismissHandler, onSubmitHandler, availableEndHours, onSelectTimeHandler, taskStartTime } = useEditDailyTaskItemForm(taskId, isModalOpen)
 
   return (
     <div className="border rounded p-4">
---
+++ src/components/TaskLists/DailyTaskList/DailyTaskListItem/EditDailyTaskItemForm/useEditDailyTaskItemForm.ts
@@ -0,0 +1,99 @@
+import { CalendarHour } from "@/application/calendarDaysGenerator/calendarDaysGenerator";
+import { findColorKey } from "@/application/findColorKey/findColorKey";
+import { cleanEventInCalendar } from "@/application/setEventInCalendar/cleanEventInCalendar";
+import { setEventInCalendar } from "@/application/setEventInCalendar/setEventInCalendar";
+import { HoursContext, SetEventInCalendarContextProps } from "@/contexts/hoursContext/hoursContext";
+import { ColorKeys, colors } from "@/domain/colors/colors";
+import { useContext, useEffect, useState } from "react";
+
+export const useEditDailyTaskItemForm = (taskId: number, isModalOpen: boolean) => {
+  const { hours, onSetHours } = useContext(HoursContext);
+  const [availableEndHours, setAvailableEndOurs] = useState<CalendarHour[]>([])
+  const [freeHours, setFreeHours] = useState<CalendarHour[]>([])
+
+  const taskHours = hours.filter((hour) => hour.taskId === taskId);
+  const taskStartTime = taskHours.at(0);
+  const taskEndTime = taskHours.at(-1);
+  const taskName = taskHours.at(0)?.eventName;
+
+  const taskColor = findColorKey(colors, taskStartTime?.color ?? "");
+
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('submit')
+    const formData = new FormData(event.currentTarget);
+    const startTime = formData.get("start-time");
+    const endTime = formData.get("end-time");
+    const taskName = formData.get("task-name");
+    const color = formData.get("color")! as string;
+
+    let colorValue = color as ColorKeys;
+
+    const setEventProps: SetEventInCalendarContextProps = {
+      color: colors[colorValue],
+      eventName: taskName as string,
+      timeStart: parseInt(startTime as string),
+      timeEnd: parseInt(endTime as string),
+    };
+
+    const cleanedHours = cleanEventInCalendar(taskId, hours)
+
+    const updatedHours = setEventInCalendar({
+      hours: cleanedHours,
+      taskId,
+      ...setEventProps,
+    });
+
+    onSetHours(updatedHours);
+
+  }
+
+  const onDismissHandler = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const onDeleteHandler = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const cleanedHours = cleanEventInCalendar(taskId, hours)
+    onSetHours(cleanedHours);
+  };
+
+  const onSelectTimeHandler = (event: React.SyntheticEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+
+    const option = event.currentTarget.value;
+    const nextTasks = hours.filter(
+      (hour) => hour.hour > parseInt(option) && hour.color && hour.taskId !== taskId
+    );
+
+    const nextTaskTime = nextTasks[0]?.hour ?? 25;
+
+    const filteredEndHours = hours.filter(
+      (hour) =>
+        !hour.color && hour.hour >= parseInt(option) && hour.hour < nextTaskTime || hour.taskId === taskId && hour.hour >= parseInt(option)
+    );
+
+    setAvailableEndOurs(filteredEndHours);
+  }
+
+
+  useEffect(() => {
+    const nextTasks = hours.filter(
+      (hour) => hour.hour > taskStartTime?.hour! && hour.color && hour.taskId !== taskId
+    );
+
+    const nextTaskTime = nextTasks[0]?.hour ?? 25;
+
+    const filteredEndHours = hours.filter(
+      (hour) =>
+        !hour.color && hour.hour >= taskStartTime?.hour! && hour.hour < nextTaskTime || hour.taskId === taskId && hour.hour >= taskStartTime?.hour!
+    );
+    setAvailableEndOurs(filteredEndHours)
+
+    const filteredFreeHours = hours.filter(hour => !hour.taskId || hour.taskId === taskId)
+    setFreeHours(filteredFreeHours)
+  }, [isModalOpen])
+
+  return { taskStartTime, onSelectTimeHandler, onSubmitHandler, onDismissHandler, onDeleteHandler, freeHours, availableEndHours, taskColor, taskEndTime, taskName }
+
+} 
