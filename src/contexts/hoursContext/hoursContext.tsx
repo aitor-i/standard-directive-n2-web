45d8fe2b266f2d3e6ff11fcfbff977e85e5ab4f1
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-13 19:46:12 +0100

    ADD - Calendar fetching hoursContex

--- src/contexts/hoursContext/hoursContext.tsx
+++ src/contexts/hoursContext/hoursContext.tsx
@@ -4,7 +4,9 @@ import {
   EventColor,
   calendarDaysGenerator,
 } from "@/application/calendarDaysGenerator/calendarDaysGenerator";
+import { FetchParams, useFetch } from "@/hooks/useFetch/useFeltch";
 import { ReactNode, createContext, useEffect, useState } from "react";
+import { redirect } from "next/navigation"
 
 export interface SetEventInCalendarContextProps {
   color: EventColor;
@@ -15,7 +17,7 @@ export interface SetEventInCalendarContextProps {
 
 export const HoursContext = createContext({
   hours: [] as CalendarHour[],
-  onSetHours: (hoursToSet: CalendarHour[]) => {},
+  onSetHours: (hoursToSet: CalendarHour[]) => { },
 });
 
 interface Props {
@@ -23,6 +25,7 @@ interface Props {
 }
 
 export function HoursContextProvider({ children }: Props) {
+  const { fetcher, response } = useFetch();
   const [hours, setHours] = useState<CalendarHour[]>([]);
   const [a, setA] = useState(false);
 
@@ -31,9 +34,29 @@ export function HoursContextProvider({ children }: Props) {
     setA((p) => !p);
   };
 
+  const fetchHandler = (token: string) => {
+
+    const url = new URL("http://localhost:4040/calendar/get-calendar-by-date")
+    const date = "2024-01-13"
+    url.searchParams.append("date", date);
+    url.searchParams.append("token", token);
+    const fetchParams: FetchParams = {
+      url: url.toString()
+    }
+
+    fetcher(fetchParams);
+
+  }
+
   useEffect(() => {
     const hoursInADay = calendarDaysGenerator();
     setHours(hoursInADay);
+    const token = window.localStorage.getItem("token");
+
+    if (!token) redirect("/login");
+    fetchHandler(token);
+
+
     // Fetch data from API
     // Set data
     //
