0a3082e7a344e9803cc3757d67810dd857f83b93
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-20 18:23:29 +0100

    ADD - tasks context

---
+++ src/contexts/hoursContext/tasksContext.tsx
@@ -0,0 +1,90 @@
+import { Task } from "@/domain/Task/Task";
+import { ApiResponse } from "@/domain/models/ApiResponse";
+import { FetchParams, useFetch } from "@/hooks/useFetch/useFeltch";
+
+import { redirect } from "next/navigation"
+
+import React, { useEffect, useState } from "react";
+
+
+export const TasksContext = React.createContext({
+  tasks: [] as Task[],
+  onSetTasks: (tasks: Task[]) => { }
+})
+
+
+interface RequestBody {
+  token: String,
+  tasks: Task[]
+}
+
+interface Props {
+  children: React.ReactNode
+}
+
+export function TasksContextProvider({ children }: Props) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const { fetcher, response, responseObject } = useFetch<ApiResponse>()
+
+  const postTasks = (token: String, tasks: Task[]) => {
+    const body: RequestBody = {
+      token,
+      tasks,
+    }
+
+    const params: FetchParams = {
+      method: 'POST',
+      url: "http://localhost:4040/tasks/save-tasks",
+      body,
+      headers: { "Content-Type": "application/json" },
+
+    }
+
+    fetcher(params)
+  }
+
+
+  const onSetTasks = (tasks: Task[]) => {
+
+    const token = window.localStorage.getItem("token");
+    if (!token) redirect("/login");
+    postTasks(token, tasks)
+
+
+    // upolad changes
+    setTasks(tasks)
+  }
+
+
+  const fetchTasks = async (token: string) => {
+
+    const url = new URL("http://localhost:4040/tasks/get-tasks")
+    url.searchParams.append("token", token);
+
+    fetch(url.toString()).then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+      .then((data: ApiResponse) => {
+        setTasks(data.tasks ?? []);
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+  }
+
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (!token) redirect("/login");
+    fetchTasks(token);
+
+  }, [])
+
+  return <TasksContext.Provider value={{ tasks: tasks, onSetTasks: onSetTasks }} >
+    {children}
+  </TasksContext.Provider >
+
+}
