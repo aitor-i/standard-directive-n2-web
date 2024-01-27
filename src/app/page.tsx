25c7356ab9076010e2f5fcc13771163602586194
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-27 22:28:14 +0200

    REFACATOR - Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import NavigationMenu from "@/components/NavigationMenu/NavigationMenu";
+import { HomePage } from "@/pages/HomePage/HomePage";
 
 export default function Home() {
-  return (
-    <main>
-      <NavigationMenu />
-      <h1>Standard directive number 2</h1>
-      <h2>Be the general</h2>
-      <h2>Be the soldier</h2>
-    </main>
-  );
+  return <HomePage />
 }
