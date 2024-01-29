65119da083c6b34bd1d373825412fe0c80db31db
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-29 20:37:20 +0100

    UPDATE - Toast in out

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react';
 import './toast.css'
 
 
 interface Props {
 
   message: string;
-  type: "success" | "neutral" | "warning" | "error"
+  type: "t-success" | "t-neutral" | "t-warning" | "t-error"
 
 }
-export function Toast({ message, type }: Props) {
+
+
+export function Toast(props: Props) {
+
+  const [toastState, setToastState] = useState<'idle' | 'toast' | 'toast-hide'>('idle')
+  const { message, type } = props;
+
+  useEffect(() => {
+    setToastState('toast')
+
+    setTimeout(() => {
+      setToastState('toast-hide')
+    }, 3000);
+  }, [props])
 
   return (
-    <div className={`overflow-clip absolute w-80 h-16 toast p-2 rounded  top-4 right-4 ${type} `}>
+    <div className={`overflow-clip absolute w-80 h-16 ${toastState} p-2 rounded  top-4 right-4 ${type} `}>
       <p className="">{message}</p>
     </div>
   )
