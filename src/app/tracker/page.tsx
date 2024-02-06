9f37c6c2805bb97ec893cfdc90cdad176acfa244
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-06 21:13:47 +0100

    ADD - tracker page

---
+++ src/app/tracker/page.tsx
@@ -0,0 +1,7 @@
+import TrackerPackage from '@/pages/trackerPage/TrackerPackage'
+import React from 'react'
+
+export default function page() {
+  return <TrackerPackage />
+
+}
---
+++ src/pages/trackerPage/TrackerPackage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TrackerPackage() {
+  return (
+    <div>TrackerPackage</div>
+  )
+}
