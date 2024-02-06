7c68f300906ef4e91a46e3ccbb0aafbd470f47f4
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-06 21:13:07 +0100

    UPDATE - User Options

--- src/components/UserOptions/UserOptions.tsx
+++ src/components/UserOptions/UserOptions.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { redirect } from "next/navigation"
 
 export const UserOptions = (props: {}) => {
+  const [showOptions, setShowOptions] = useState(false)
+
+  const changeOpenStatusHandler = () => {
+    setShowOptions(prev => !prev)
+
+  }
+
+  const logOutHandler = () => {
+    console.log("log out")
+    window.localStorage.removeItem("token");
+    redirect("/");
+
+  }
   return (
-    <div className='bg-white debug p-2 flex flex-col gap-4'>
-      <div className='debug w-24 h-24 self-center rounded-full flex justify-center align-middle '>Foto</div>
-      <div>
-        <ul>
-          <li>Log out</li>
-          <li>Cange password</li>
-          <li>Change image</li>
-        </ul>
-      </div>
+    <div className='p-2 cursor-pointer flex flex-col gap-4 w-36'>
+      <p onClick={changeOpenStatusHandler}>Aitor Ibarra</p>
+      {
+        showOptions ?
+          <div>
+            <ul>
+              <li onClick={logOutHandler}>Log out</li>
+              <li>Cange password</li>
+              <li>Change image</li>
+            </ul>
+          </div> : null
+      }
     </div>
   )
 }
