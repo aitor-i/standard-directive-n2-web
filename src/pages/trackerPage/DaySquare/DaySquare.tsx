56e341b9bd68428201b659048fc81a53769edfe7
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-08 20:21:22 +0100

    UPDATE - Day ssquare styles

--- src/pages/trackerPage/DaySquare/DaySquare.tsx
+++ src/pages/trackerPage/DaySquare/DaySquare.tsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from "react";
+
 type SquareProps = {
   color: string;
+  isCompleted: boolean
+  day: number
 };
 
-export const DaySquare: React.FC<SquareProps> = ({ color }) => {
+export const DaySquare = ({ color, isCompleted, day }: SquareProps) => {
+  const [isDateVisible, setIsDateVisible] = useState(false)
+
+  const onBoxClickHandler = () => {
+    setIsDateVisible(true)
+  }
+
+  if (isDateVisible) {
+    setTimeout(() => {
+      setIsDateVisible(false)
+    }, 2000);
+  }
+
+
+
   return (
-    <div
-      style={{
-        width: '20px',
-        height: '20px',
-        backgroundColor: color,
-        display: 'inline-block',
-        margin: '2px',
-      }}
-    ></div>
+    <div className="relative h-min p-0 m-0  w-min">
+      <div
+        onClick={onBoxClickHandler}
+        style={{
+          width: '20px',
+          height: '20px',
+          backgroundColor: isCompleted ? color : '#E5E5E5',
+          display: 'inline-block',
+          margin: '2px',
+          borderRadius: '5px',
+          cursor: 'pointer',
+
+        }}
+      ></div>
+      {isDateVisible && <p className="absolute" >{day}</p>}
+    </div>
   );
 };
