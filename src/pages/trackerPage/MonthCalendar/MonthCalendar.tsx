6ed66ed46ec357dbf5e4b084ffdb7d72a0de9651
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-09 04:15:48 +0100

    UPDATE - Month calendar

--- src/pages/trackerPage/MonthCalendar/MonthCalendar.tsx
+++ src/pages/trackerPage/MonthCalendar/MonthCalendar.tsx
@@ -1,60 +1,41 @@
 'use client'
-import { DayObject, generateDayObjectsForCurrentMonth } from "@/application/generateDayObjectsForCurrentMonth/generateDayObjectsForCurrentMonth";
+import { DayObject } from "@/application/generateDayObjectsForCurrentMonth/generateDayObjectsForCurrentMonth";
 import { DaySquare } from "../DaySquare/DaySquare";
-import { useEffect, useState } from "react";
 
 
 interface Props {
 
   tasksName: String
+  days: DayObject[]
+  trackId: string
+  onCompleteClicl: (trackId: string) => void
+
 }
 
-export const MonthCalendar = ({ tasksName }: Props) => {
-  const [daysInMonth, setdaysInMonth] = useState<DayObject[]>([]);
-
-  const today = new Date();
-
-  const onclickCompleteHandler = () => {
-
-    const updatedDays = daysInMonth.map((day) => {
-      if (day.day === today.getDate()) day.completed = true;
-      return day
-    })
-
-    setdaysInMonth(updatedDays)
-  }
+export const MonthCalendar = ({ tasksName, days, trackId, onCompleteClicl }: Props) => {
 
   const getCompletedPercentage = (days: DayObject[]) => {
-
     const completed = days.reduce((prevValue, day) => {
-
       if (day.completed) return prevValue + 1;
       return prevValue;
-
     }, 0)
-
     return (completed / days.length).toFixed(2)
   }
 
-  const percentage = +getCompletedPercentage(daysInMonth) * 100
-
-  useEffect(() => {
-
-    const daysOfTheMonth = generateDayObjectsForCurrentMonth("#ff5252")
-    setdaysInMonth(daysOfTheMonth);
-  }, [])
+  const percentage = +getCompletedPercentage(days) * 100
 
   return <div className="flex items-center h-min gap-4">
     <p>{tasksName}</p>
     <div className="flex items-center h-min">
       {
-        daysInMonth.map((day) => {
+        days.map((day) => {
           return <DaySquare key={day.day} color={day.color} day={day.day} isCompleted={day.completed} />
         })
       }
     </div>
 
     <p>%{percentage}</p>
-    <button className="primary" onClick={onclickCompleteHandler}>!</button>
+    <p>{trackId}</p>
+    <button className="primary" onClick={() => onCompleteClicl(trackId)}>!</button>
   </div>;
 };
