ce8a065ac407dbdeca74f177a7e6682088ccecce
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-09 04:16:09 +0100

    ADD - New tracker form

--- src/pages/trackerPage/TrackerPackage.tsx
+++ src/pages/trackerPage/TrackerPackage.tsx
@@ -1,21 +1,54 @@
 'use client'
 import NavigationMenu from '@/components/NavigationMenu/NavigationMenu'
 import { UserOptions } from '@/components/UserOptions/UserOptions'
-import React from 'react'
+import React, { useState } from 'react'
 import { MonthCalendar } from './MonthCalendar/MonthCalendar';
+import { DayObject, generateDayObjectsForCurrentMonth } from "@/application/generateDayObjectsForCurrentMonth/generateDayObjectsForCurrentMonth";
+import { generateRandomID } from '@/application/generateRandomId/generateRandomId';
+
+
+interface TrackerObject {
+  title: String,
+  id: string,
+  days: DayObject[]
+}
 
 export default function TrackerPackage() {
+  const [trackers, setTrackers] = useState<TrackerObject[]>([])
 
   const onSubmitNewTrackerHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     const formData = new FormData(event.currentTarget);
 
-    const title = formData.get('title')?.valueOf()
+    const title = formData.get('title')?.valueOf() ?? ''
     const color = formData.get('color')?.valueOf() ?? 'red'
 
+    const days = generateDayObjectsForCurrentMonth(color.toString())
+    const id = generateRandomID();
+    const tracker: TrackerObject = {
+      title: title?.toString(),
+      id,
+      days
+    }
 
-    console.log(title, color)
+    setTrackers(trackers => [...trackers, tracker])
+  }
+
+  const onCompletedCkickHandler = (trackId: string) => {
+    debugger
+
+    const updatedDays: DayObject[] = trackers.filter(track => track.id === trackId)[0].days.map((day) => {
+      const today = new Date()
+      if (day.day === today.getDate()) day.completed = true;
+      return day
+    })
+
+    const updatedTrackers: TrackerObject[] = trackers.map((tracker) => {
+      if (tracker.id === trackId) tracker.days = updatedDays;
+      return tracker
+    })
+
+    setTrackers(updatedTrackers)
 
   }
 
@@ -33,9 +66,7 @@ export default function TrackerPackage() {
         </div>
       </div>
       <div className="flex flex-1 flex-col">
-        <MonthCalendar tasksName='Node' />
-        <MonthCalendar tasksName='React' />
-        <MonthCalendar tasksName='Rust' />
+        {trackers.map(track => <MonthCalendar trackId={track.id} tasksName={track.title} onCompleteClicl={onCompletedCkickHandler} key={track.id} days={track.days} />)}
       </div>
 
       <h5>Add tracker</h5>
