9696fa70d8aea7941cc8708326864b88bbdd102e
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-10 16:38:28 +0100

    ADD - Save tracks actions

--- src/pages/trackerPage/TrackerPackage.tsx
+++ src/pages/trackerPage/TrackerPackage.tsx
@@ -1,20 +1,50 @@
 'use client'
 import NavigationMenu from '@/components/NavigationMenu/NavigationMenu'
 import { UserOptions } from '@/components/UserOptions/UserOptions'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MonthCalendar } from './MonthCalendar/MonthCalendar';
 import { DayObject, generateDayObjectsForCurrentMonth } from "@/application/generateDayObjectsForCurrentMonth/generateDayObjectsForCurrentMonth";
 import { generateRandomID } from '@/application/generateRandomId/generateRandomId';
+import { colors } from "@/domain/colors/colors";
+import { FetchParams, useFetch } from '@/hooks/useFetch/useFeltch';
+import { redirect } from "next/navigation"
 
 
 interface TrackerObject {
-  title: String,
+  title: string,
   id: string,
   days: DayObject[]
 }
 
+
+interface SaveTrackersBody {
+
+  trackers: TrackerObject[],
+  token: string
+}
+
 export default function TrackerPackage() {
   const [trackers, setTrackers] = useState<TrackerObject[]>([])
+  const { fetcher: poster, response: postResponse, fetchingStatus: postingStatus } = useFetch();
+  const baseUrl = process.env.NEXT_PUBLIC_AUTH_API_BASE_URL;
+
+  const saveTrackersHander = (token: string, trackers: TrackerObject[]) => {
+
+    const body: SaveTrackersBody = {
+      trackers,
+      token
+    }
+
+    const url = new URL(`${baseUrl}/trackers/save-trackers`)
+    const fetchParams: FetchParams = {
+      url: url.toString(),
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body
+    }
+
+    poster(fetchParams)
+  }
 
   const onSubmitNewTrackerHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -34,6 +64,7 @@ export default function TrackerPackage() {
     setTrackers(trackers => [...trackers, tracker])
   }
 
+
   const onCompletedCkickHandler = (trackId: string) => {
     const updatedDays: DayObject[] = trackers.filter(track => track.id === trackId)[0].days.map((day) => {
       const today = new Date()
@@ -48,14 +79,25 @@ export default function TrackerPackage() {
 
     setTrackers(updatedTrackers)
 
+    const token = window.localStorage.getItem("token");
+    if (!token) redirect("/login");
+    saveTrackersHander(token, trackers)
+
   }
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (!token) redirect("/login");
+    saveTrackersHander(token, trackers)
+
+  }, [trackers])
 
   const dateString = new Date().toDateString();
   return (
     <main className="h-full flex flex-col">
       <div
         key="header"
-        className="flex justify-around border-b-2 border-stdSlateGray w-full pb-4 pt-2"
+        className="flex justify-around border-b-2 border-stdSlateGray  w-full pb-4 pt-2"
       >
         <p>{dateString}</p>
         <NavigationMenu />
@@ -65,19 +107,25 @@ export default function TrackerPackage() {
       </div>
       <div className="flex flex-1 flex-col">
         {trackers.map(track => <MonthCalendar trackId={track.id} tasksName={track.title} onCompleteClick={onCompletedCkickHandler} key={track.id} days={track.days} />)}
+
+        <div className='self-center'>
+          <h5>Add tracker</h5>
+          <form className='flex' onSubmit={onSubmitNewTrackerHandler}>
+            <input type="text" name="title" placeholder='Enter title' required />
+            <select name='color'>
+              {Object.keys(colors).map((colorName) => (
+                <option key={colorName} value={colorName}>
+                  {colorName}
+                </option>
+              ))}
+            </select>
+            <div>
+              <button className='primary' type='submit' >Add</button>
+            </div>
+          </form>
+        </div>
       </div>
 
-      <h5>Add tracker</h5>
-      <form className='flex ' onSubmit={onSubmitNewTrackerHandler}>
-        <input type="text" name="title" placeholder='Enter title' required />
-        <select name='color'>
-          <option value={'red'}>Red</option>
-          <option value={'green'}>Green</option>
-        </select>
-        <div>
-          <button className='primary' type='submit' >Add</button>
-        </div>
-      </form>
     </main>
   )
 }
