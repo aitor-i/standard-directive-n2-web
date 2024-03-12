50af9075de5321c0df94d6db11a8b749f50c8ef6
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-12 20:22:01 +0100

    ADD - Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="p-4 bg-gray-700 text-center">
+      <p className='text-sm  text-white'>Planning and discipline are the bedrock of success. Start your journey today with our comprehensive calendar tool.</p>
+    </footer>
+  )
+}
